3efc031520d2651bc6bfd6f19e4a678afc734079
champiao <rafael.leite.assis63@gmail.com>
2020-11-25 14:12:02 -0300

    .

--- tabuada.py
+++ tabuada.py
@@ -2,17 +2,21 @@ from tqdm import tqdm
 from time import sleep
 import math
 def menu():
-    num = int(input('digite um numero de 1 a 9: ')) 
+    numIn = input('digite um numero de 1 a 9: ')
     contador = 1
+    if numIn == "":
+        print('não é um número valido para uma tabuada')
+        return menu()
+    num = int(numIn)
     if num != 0 and num <= 10:
         while contador != 11:
             resultado = num * contador
             print(str(num) + ' X ' + str(contador) + ' = ' + str(resultado))
             contador = contador + 1
     if num <= 0 or num > 10 or num == "":
-        print('nao e um numero valido para uma taboada')
+        print('não é um número valido para uma tabuada')
         return menu()
-    opcao = str(input('gostaria de fazer mais alguma taboada?: '))
+    opcao = str(input('gostaria de fazer mais alguma tabuada?: '))
     if opcao == 'não' or opcao == 'nao':
         print('good bye')
         exit()
@@ -21,7 +25,7 @@ def menu():
     if opcao != 'sim' and opcao != 'nao' and opcao != 'não':
         print('opção invalida!.... saindo do programa por inserção de dados inválidos')
         exit()
-    if opcao:
+    elif opcao == "":
         print('opção invalida!.... saindo do programa por inserção de dados inválidos')
         exit()
 menu();
